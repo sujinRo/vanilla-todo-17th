6966b0673899a3d62e92992a2dc07c8047d11fe7
sujinRo <shtnwls1111@ewhain.net>
2025-03-10 22:29:25 +0900

    feat: count 추가

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const todoInput = document.querySelector('#inputPlan'),
   todoList = document.querySelector('#todoList'),
   doneList = document.querySelector('#doneList'),
-  todoCount = document.querySelector('.todoCount'),
-  doneCount = document.querySelector('.doneCount');
-
+  todoCount = document.querySelector('#todoCount'),
+  doneCount = document.querySelector('#doneCount');
 //enter을 누르면 input 내용이 to do 섹션으로 넘어감
 // function enterkey() {
 //   if (window.event.keyCode == 13) {
@@ -23,8 +22,8 @@ function handleAddBtnClick() {
   button.innerText = '❎';
 
   button.className = 'deleteBtn';
-  button.addEventListener('click', () => {
-    handleDeleteBtnClick(todoList);
+  button.addEventListener('click', (event) => {
+    handleDeleteBtnClick(event, 'todoList');
   });
 
   text.innerHTML = input;
@@ -37,6 +36,7 @@ function handleAddBtnClick() {
 
   if (todoInput.value != '') {
     todoList.appendChild(addList);
+    addCount('todoList');
     todoInput.value = '';
   } else {
     alert('No item!');
@@ -45,39 +45,68 @@ function handleAddBtnClick() {
 
 function handleItemClick(text, button, parentClassName) {
   let list = document.createElement('li');
-  let moveText = document.createElement('text');
+  let moveText = document.createElement('span');
   let btn = document.createElement('button');
 
-  moveText = text;
-  btn = button;
-  list.append(moveText, btn);
+  moveText.innerText = text.innerText;
+  btn.innerText = button.innerText;
+  btn.className = 'deleteBtn';
 
-  //todoList섹션에서 text를 누르면 doneList섹션으로 넘어감
+  list.append(moveText, btn);
+  list.className = 'addList';
+
   if (parentClassName == 'todoList') {
-    btn.addEventListener('click', () => {
-      handleDeleteBtnClick(doneList);
+    btn.addEventListener('click', (event) => {
+      handleDeleteBtnClick(event, 'doneList');
     });
     moveText.addEventListener('click', () => {
       handleItemClick(moveText, btn, 'doneList');
     });
 
+    text.parentElement.remove();
+    minusCount('todoList');
+
     doneList.appendChild(list);
-  }
-  //doneList섹션에서 text를 누르면 todoList섹션으로 넘어감
-  else if (parentClassName == 'doneList') {
-    btn.addEventListener('click', () => {
-      handleDeleteBtnClick(todoList);
+    addCount('doneList');
+  } else if (parentClassName == 'doneList') {
+    btn.addEventListener('click', (event) => {
+      handleDeleteBtnClick(event, 'todoList');
     });
     moveText.addEventListener('click', () => {
       handleItemClick(moveText, btn, 'todoList');
     });
 
+    text.parentElement.remove();
+    minusCount('doneList');
+
     todoList.appendChild(list);
+    addCount('todoList');
+  }
+}
+
+function addCount(list) {
+  if (list == 'todoList') {
+    let todoCurrentCount = parseInt(todoCount.innerText) + 1;
+    todoCount.innerText = todoCurrentCount;
+  } else if (list == 'doneList') {
+    let doneCurrentCount = parseInt(doneCount.innerText) + 1;
+    doneCount.innerText = doneCurrentCount;
+  }
+}
+
+function minusCount(list) {
+  if (list == 'todoList') {
+    let todoCurrentCount = parseInt(todoCount.innerText) - 1;
+    todoCount.innerText = todoCurrentCount;
+  } else if (list == 'doneList') {
+    let doneCurrentCount = parseInt(doneCount.innerText) - 1;
+    doneCount.innerText = doneCurrentCount;
   }
 }
 
 //text옆 ❎버튼을 누르면 내용 delete
-function handleDeleteBtnClick(listName) {
+function handleDeleteBtnClick(event, listName) {
   const list = event.target.parentElement;
   list.remove();
+  minusCount(listName == 'todoList' ? 'todoList' : 'doneList');
 }
